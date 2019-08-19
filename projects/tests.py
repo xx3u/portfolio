ca921cb8e28aeea253d31c25fccc09a95e4e5465
Yuliya <yuliyahvan@yahoo.com>
2019-08-20 01:22:42 +0600

    Update porjects

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from projects.models import Project
+import pytest
 
-# Create your tests here.
+
+#class ProjectModelTest(TestCase):
+#    def setUp(self):
+#        project = Project.objects.create(
+#            title='My 1st Project: Alashop.kz', description='A web development framework',
+#            technology='Flask, Telegram bot', link='http')
+project = Project.objects.create(
+    title='My 1st Project: Alashop.kz',
+    description='A web development framework',
+    technology='Flask, Telegram bot', link='http'
+)
+
+
+def test_project_create(db):
+    assert str(project.title) == 'My 1st Project: Alashop.kz'
+
+
+def test_admin(db):
+    assert str()
